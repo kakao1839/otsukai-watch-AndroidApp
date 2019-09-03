148b194e07fa720bd08ee467ceff80152136fcc8
kakao <kwt20018@gmail.com>
2019-09-03 17:20:06 +0900

    commit

--- app/src/main/java/com/example/otukai_watch/MainYarukotoListActivity.kt
+++ app/src/main/java/com/example/otukai_watch/MainYarukotoListActivity.kt
@@ -2,8 +2,11 @@ package com.example.otukai_watch
 
 import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 import kotlinx.android.synthetic.main.activity_main_yarukotolist.*
 
@@ -14,14 +17,25 @@ class MainYarukotoListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_yarukotolist)
         setSupportActionBar(toolbar)
 
+        val fab = findViewById<View>(R.id.fab) as FloatingActionButton
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            //Show Dialog here to add new Item
+            addNewItemDialog()
         }
 
         StartOtukaiButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun addNewItemDialog() {
+        val alert = AlertDialog.Builder(this)
+        val itemEditText = EditText(this)
+        alert.setTitle("やることを追加")
+        alert.setMessage("商品名を入力する")
+        alert.setView(itemEditText)
+        alert.setPositiveButton("Submit") { dialog, positiveButton ->   }
+        alert.show()
     }
 }
