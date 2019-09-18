dbd38309d0e4544653249f6ca28fabd2c6bbd2b3
THINKPAD-KAKAO <kwt20018@gmail.com>
2019-09-18 15:41:24 +0900

    ✍commit

--- app/src/main/java/com/example/otukai_watch/ToDoList/DBHandler.kt
+++ app/src/main/java/com/example/otukai_watch/ToDoList/DBHandler.kt
@@ -4,7 +4,6 @@ import android.content.ContentValues
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
-import com.example.otukai_watch.ToDoList.DTO.ToDo
 import com.example.otukai_watch.ToDoList.DTO.ToDoItem
 
 
---
+++ app/src/main/java/com/example/otukai_watch/ToDoList/Task.kt
@@ -0,0 +1,6 @@
+package com.example.otukai_watch.ToDoList
+
+data class Task (
+    val user: String,
+    val item: String
+)
