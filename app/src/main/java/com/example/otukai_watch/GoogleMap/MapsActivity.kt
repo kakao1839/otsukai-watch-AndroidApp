02c8fa1bc7ba41ca4c85d0c2303bd63ef567b7a5
THINKPAD-KAKAO <kwt20018@gmail.com>
2019-09-20 13:34:21 +0900

    🛠commit

--- app/src/main/java/com/example/otukai_watch/GoogleMap/MapsActivity.kt
+++ app/src/main/java/com/example/otukai_watch/GoogleMap/MapsActivity.kt
@@ -17,10 +17,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.navigation_home) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
