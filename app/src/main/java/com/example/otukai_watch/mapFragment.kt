7a9948459c95fff97fb0e99209675cfc215ad26d
kakao <kwt20018@gmail.com>
2019-11-03 03:26:38 +0900

    mokkai

--- app/src/main/java/com/example/otukai_watch/mapFragment.kt
+++ app/src/main/java/com/example/otukai_watch/mapFragment.kt
@@ -1,11 +1,7 @@
 package com.example.otukai_watch
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
