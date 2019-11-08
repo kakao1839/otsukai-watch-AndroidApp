656459650799d982e5a6dc506b485d52a1ce00c7
THINKPAD-KAKAO <kwt20018@gmail.com>
2019-11-09 01:51:24 +0900

    VCのデザイン変更

--- app/src/main/java/com/example/otukai_watch/MainActivity.kt
+++ app/src/main/java/com/example/otukai_watch/MainActivity.kt
@@ -2,9 +2,11 @@ package com.example.otukai_watch
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.otukai_watch.ToDoList.ItemActivity
+import com.example.otukai_watch.ToDoList.Task
 import com.example.otukai_watch.VoiceChat.VoiceActivity
 import com.github.kittinunf.fuel.httpGet
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -17,8 +19,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.github.kittinunf.fuel.httpPost
 import com.github.kittinunf.result.Result
+import com.squareup.moshi.JsonAdapter
 import com.squareup.moshi.Moshi
+import com.squareup.moshi.Types
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import kotlinx.android.synthetic.main.activity_item.*
 import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
@@ -26,102 +31,95 @@ import java.util.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var textMessage: TextView
-    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                val intent = Intent(this, VoiceActivity::class.java)
-                startActivity(intent)
-            }
-            R.id.navigation_notifications -> {
-                val intent = Intent(this, ItemActivity::class.java)
-                startActivity(intent)
-            }
+  private lateinit var textMessage: TextView
+  private val onNavigationItemSelectedListener =
+    BottomNavigationView.OnNavigationItemSelectedListener { item ->
+      when (item.itemId) {
+        R.id.navigation_home -> {
+          return@OnNavigationItemSelectedListener true
         }
-        false
+        R.id.navigation_dashboard -> {
+          val intent = Intent(this, VoiceActivity::class.java)
+          startActivity(intent)
+        }
+        R.id.navigation_notifications -> {
+          val intent = Intent(this, ItemActivity::class.java)
+          startActivity(intent)
+        }
+      }
+      false
     }
 
-    private lateinit var mMap: GoogleMap
+  private lateinit var mMap: GoogleMap
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+    val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
-    }
+    // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+    val mapFragment = supportFragmentManager
+      .findFragmentById(R.id.map) as SupportMapFragment
+    mapFragment.getMapAsync(this)
+  }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
+  /**
+   * Manipulates the map once available.
+   * This callback is triggered when the map is ready to be used.
+   * This is where we can add markers or lines, add listeners or move the camera. In this case,
+   * we just add a marker near Sydney, Australia.
+   * If Google Play services is not installed on the device, the user will be prompted to install
+   * it inside the SupportMapFragment. This method will only be triggered once the user has
+   * installed Google Play services and returned to the app.
+   */
+  override fun onMapReady(googleMap: GoogleMap) {
 
-        // API URL
-        val requestUrl = "https://pck.itok01.com/api/v1/location?user=taro"
+    // API URL
+    val requestUrl = "https://pck.itok01.com/api/v1/location"
 
-        mMap = googleMap
+    mMap = googleMap
 
-        data class MapTask (
-            val user: String,
-            val latitude:Float,
-            val longitude:Float
-        )
+    val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
 
-        val moshi = Moshi.Builder().add(KotlinJsonAdapterFactory()).build()
-        val requestAdapter = moshi.adapter(MapTask::class.java)
-        val header: HashMap<String, String> = hashMapOf("Content-Type" to "application/json")
+    val httpAsync = (requestUrl + "?user=taro")
+      .httpGet()
+      .responseString { request, response, result ->
+        Log.d("hoge", result.toString())
+        when (result) {
+          is Result.Failure -> {
+            val ex = result.getException()
+            println(ex)
+          }
+          is Result.Success -> {
+            val data = result.get()
+            println(data)
+            val pos = moshi.adapter(Position::class.java).fromJson(data)
 
-        val maptask = MapTask(
-            user = "taro",
-            latitude = 26.5262305,
-            longitude = 128.0293976
-        )
+            //val basyo = LatLng(pos!!.latitude, pos!!.longitude) //緯度,経度
+            val basyo = LatLng(pos!!.latitude, pos!!.longitude) //緯度,経度
+            val zoomValue = 13.0f // 1.0f 〜 21.0f を指定
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(basyo, zoomValue))
+            //   mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(basyo, zoomValue))
 
-        val httpAsync = requestUrl
-            .httpPost()
-            .header(header)
-            .body(requestAdapter.toJson(maptask))
-            .responseString { request, response, result ->
-                when (result) {
-                    is Result.Failure -> {
-                        val ex = result.getException()
-                        println(ex)
-                    }
-                    is Result.Success -> {
-                        val data = result.get()
-                        println(data)
-                    }
-                }
-            }
-        httpAsync.join()
-
-        val basyo = LatLng(26.526387, 128.028866) //緯度,経度
-        val zoomValue = 13.0f // 1.0f 〜 21.0f を指定
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(basyo, zoomValue))
-     //   mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(basyo, zoomValue))
-
-        val marker = mMap.addMarker(
-            MarkerOptions()
+            val marker = mMap.addMarker(
+              MarkerOptions()
                 .position(basyo)
                 .title("現在地")    // マーカーをタップ時に表示するテキスト文字列
                 .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
-        )
-        marker.showInfoWindow() // タップした時と同じ挙動
+            )
+            marker.showInfoWindow() // タップした時と同じ挙動
+          }
+        }
+      }
+
+    httpAsync.join()
 
 
-    }
+  }
+
+  data class Position(val user: String, val latitude: Double, val longitude: Double)
+
+
 }
